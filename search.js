c9fb57ad9e1eae26c37552cb19eb2999b3bbd927
AarushBhagwat <86906061+AarushBhagwat@users.noreply.github.com>
2021-07-19 11:50:05 +0530

    Add files via upload

---
+++ search.js
@@ -0,0 +1,30 @@
+function openPage(){
+    var x = document.getElementById("search").value;
+
+    if (x === "html") {
+              window.open("HTML.html");
+        }
+        if (x === "HTML") {
+              window.open("HTML.html");
+        }
+              if (x === "english") {
+              window.open("english.html");
+              }
+              if (x === "maths") {
+              window.open("maths.html");
+        }
+
+        if (x === "calculators") {
+            window.open("calc.html");
+      }
+      if (x === "tags") {
+            window.open("tags.html");
+      }
+            if (x === "lesson-1") {
+            window.open("tq.html");
+            }
+            if (x === "home") {
+            window.open("index.html");
+      }
+
+  }
